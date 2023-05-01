52a7bcd74a64044d970140bc928795c2aeeaa3a3
JayDaMan53 <118361901+JayDaMan53@users.noreply.github.com>
2023-05-01 18:22:36 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const list1 = ['so pog', 'item2', 'item3'];
-const list2 = ['itemA', 'itemB', 'itemC'];
+const list2 = ['yay', 'itemB', 'itemC'];
 const list3 = ['itemX', 'itemY', 'itemZ'];
 
 window.myLists = {
